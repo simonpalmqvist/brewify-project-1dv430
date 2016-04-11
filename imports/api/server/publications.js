40099ec6ecf3ea3fdaeeb47389ef2a1fb759a685
Simon Palmqvist <simon.palmqvist@gmail.com>
2016-04-11 17:44:52 +0200

    Publishing breweryDB collections

--- imports/api/server/publications.js
+++ imports/api/server/publications.js
@@ -1,5 +1,5 @@
 /**
- * @description Module to publish applications data sources
+ * @description Module to publish application data sources (what the client should be able to see)
  * @author simonpalmqvist
  */
 
@@ -8,11 +8,60 @@ import { Meteor } from "meteor/meteor";
 
 //Collections
 import { Recipes } from "../recipes/Recipes";
+import { Hops } from "../brewerydb/Hops";
+import { Fermentables } from "../brewerydb/Fermentables";
+import { Yeasts } from "../brewerydb/Yeasts";
+import { Ingredients } from "../brewerydb/Ingredients";
+import { Styles } from "../brewerydb/Styles";
 
 //Define what should be published to the client
+
+//All recipes that belong to user
 Meteor.publish("recipes", function() {
     if (!this.userId) {
         return null;
     }
     return Recipes.find({userId: this.userId });
+});
+
+//All hops for logged in users
+Meteor.publish("hops", function() {
+    if (!this.userId) {
+        return null;
+    }
+    return Hops.find({});
+});
+
+//All Fermentables for logged in users
+Meteor.publish("fermentables", function() {
+    if (!this.userId) {
+        return null;
+    }
+    return Fermentables.find({});
+});
+
+//All Yeasts for logged in users
+Meteor.publish("yeasts", function() {
+    if (!this.userId) {
+        return null;
+    }
+    return Yeasts.find({});
+});
+
+//All Other ingredients for logged in users
+Meteor.publish("ingredients", function() {
+    if (!this.userId) {
+        return null;
+    }
+    //Ingredients also contain hops, fermentables and yeasts so only return misc
+    return Ingredients.find({category: "misc"});
+});
+
+//All beer styles for logged in users
+Meteor.publish("styles", function() {
+    if (!this.userId) {
+        return null;
+    }
+
+    return Styles.find({});
 });
