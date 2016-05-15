83bbdf5c0c3be945a3f629faf2ff25624f89e9e1
Simon Palmqvist <simon.palmqvist@gmail.com>
2016-05-15 15:30:05 +0200

    Adding dropdown menu

--- imports/ui/components/navigation/NavigationBar.jsx
+++ imports/ui/components/navigation/NavigationBar.jsx
@@ -7,18 +7,45 @@ import React from "react";
 
 import NavigationItem from "./NavigationItem";
 import BackButton from "../base/BackButton";
+import { Link } from "react-router";
 
 export default class NavigationBar extends React.Component {
+    constructor() {
+        super();
+
+        this.state = ({listOpen: false});
+    }
+
+    toggleMenu() {
+        //Toggle menu
+        this.setState({listOpen: !this.state.listOpen});
+    }
+
+    minimizeMenu() {
+        this.setState({listOpen: false});
+    }
+
     render() {
+        const { listOpen } = this.state;
 
-        let links = [
-            {url: "/register", title: "Register"},
-            {url: "/login", title: "Login"}
-        ];
+        //Show menu
+        let nav = (<a href="#" onClick={this.toggleMenu.bind(this)}>Menu</a>);
+        let list;
 
-        links = Meteor.userId() ? [{url: "/logout", title: "Logout"}] : links;
+        if (!Meteor.userId()) {
+            nav = (<Link to="/login">Login</Link>);
+        } else if(listOpen) {
+            list = (
+                <ul className="expanded-list">
+                    <li><Link to="/brew/profile">Brew profile</Link></li>
+                    <li><Link to="/logout">Logout</Link></li>
+                </ul>
+            );
+        }
 
-        links = links.map((props, i) => (<NavigationItem key={i} {...props}/>));
+        //links = Meteor.userId() ? [{url: "/logout", title: "Logout"}] : links;
+
+        //links = links.map((props, i) => (<NavigationItem key={i} {...props}/>));
 
         return (
             <header className="c-navigation-bar">
@@ -26,11 +53,12 @@ export default class NavigationBar extends React.Component {
                     <BackButton/>
                     <img className="logo" src="/logo.png" alt="Brewify"/>
                     <nav>
-                        <ul>
-                            {links}
-                        </ul>
+                        { nav }
+                        { list }
                     </nav>
+
                 </div>
+
             </header>
 
         );
